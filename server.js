ad4eb8ad740db1486f4d44a32fe8ad5083fd486d
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2020-08-16 12:47:42 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+//jshint esversion:6
+
+const express = require('express')
+const ejs = require('ejs')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use(express.static(__dirname + "/public"))
+
+app.use("/public", express.static('public'))
+
+app.get('/portfolio', function(req, res) {
+  res.render('portfolio')
+})
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000')
+})
